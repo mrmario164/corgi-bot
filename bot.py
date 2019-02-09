0c7db2345c7febf294b7595133dd973e3a6ad322
mrmario164 <47465636+mrmario164@users.noreply.github.com>
2019-02-09 14:29:29 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,17 @@
+#https://discordapp.com/api/oauth2/authorize?client_id=543567651130310666&permissions=8&scope=bot
+
 import discord
 from discord.ext import commands
 import asyncio
 from itertools import cycle
+import random
 
 client = commands.Bot(command_prefix = "c!")
+client.remove_command('help')
 
 TOKEN = 'NTQzNTY3NjUxMTMwMzEwNjY2.Dz-e-w.lKqjY0UFpJM--4llZhZA3RKTjbY'
 
-statusmsg = ["c!help - Get to know server commands with this one! WOOF!!"]
+statusmsg = ["c!help - Get to know server commands with this one!"]
 
 async def change_status():
     await client.wait_until_ready()
@@ -20,11 +24,18 @@ async def change_status():
 
 @client.event
 async def on_ready():
-    print("I'm a cooooooooooorgi!! Ready to go!")
+    print("I'm a corgi!! Ready to go!")
 
 @client.command(pass_context=True)
-async def ping(ctx):
-    await client.say("Pong!")
+async def help(ctx):
+    author = ctx.message.author
+    embed = discord.Embed(
+        colour = discord.Colour.blue()
+    )
+    embed.set_author(name='Corgi-Bot - Help and Documentation')
+    embed.add_field(name='!help', value='Tells you about all the commands', inline=False)
+    await client.send_message(author, embed=embed)
+    await client.say("I've sent you a DM containing everything :ok_hand:")
 
 client.loop.create_task(change_status())
 client.run(TOKEN)
